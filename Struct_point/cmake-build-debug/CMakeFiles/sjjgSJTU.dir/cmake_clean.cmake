8bd5c536b3095856fce79ff35d6a26f10ecbd7af
touchwolf <U201310343@hust.edu.cn>
2018-08-14 00:29:41 +0800

    C++中的结构体Struct和指针的部分使用

---
+++ Struct_point/cmake-build-debug/CMakeFiles/sjjgSJTU.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sjjgSJTU.dir/main.cpp.obj"
+  "sjjgSJTU.pdb"
+  "sjjgSJTU.exe"
+  "sjjgSJTU.exe.manifest"
+  "libsjjgSJTU.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sjjgSJTU.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
